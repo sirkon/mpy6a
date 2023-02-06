12ae305c25b66c1f231bea8bb65b57e5affb9c06
Denis Cheremisov <denis.cheremisov@gmail.com>
2023-02-06 16:31:16 +0300

    fix: atomic amd64 load and store

--- internal/types/index_amd64.s
+++ internal/types/index_amd64.s
@@ -1,22 +1,27 @@
-TEXT ·setIndexAtomic(SB),NOSPLIT,$0-24
+TEXT ·setIndexAtomic(SB),$0-24
     MOVQ ptr+0(FP), BP
     MOVQ ptr+8(FP), BX
     MOVQ ptr+16(FP), CX
     MOVQ 0(BP), AX
     MOVQ 8(BP), DX
 setIndexLoop:
-    LOCK CMPXCHG16B (BP)
+    LOCK  
+    CMPXCHG16B(BP)
     JE done
     PAUSE
     JMP setIndexLoop
+done:
     RET
 
-TEXT ·getIndexAtomic(SB),NOSPLIT,$0-24
+TEXT ·getIndexAtomic(SB),$0-24
     MOVQ ptr+0(FP), BP
     XORQ AX, AX
     XORQ BX, BX
     XORQ CX, CX
     XORQ DX, DX
-    LOCK CMPXCHG16B (BP)
+    LOCK 
+    CMPXCHG16B (BP)
     MOVQ AX, ptr+8(FP)
-    MOVQ DX, ptr+17(FP)
+    MOVQ DX, ptr+16(FP)
+    RET
+
