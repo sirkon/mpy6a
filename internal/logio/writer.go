0b402839dd1ddc2165f9f1c7e7012ca808fde2d6
Denis Cheremisov <denis.cheremisov@gmail.com>
2023-04-13 21:39:02 +0300

    fix: wip

--- internal/logio/writer.go
+++ internal/logio/writer.go
@@ -1,6 +1,7 @@
 package logio
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/binary"
 	"io"
@@ -15,9 +16,9 @@ import (
 // NewWriter конструктор новой писалки в файл.
 // Параметры:
 //
-//  - name имя файла. Если он уже существует, то будет переоткрыт на чтение и запись.
-//  - frame размер кадра. Если файл существует, то этот параметр будет взят из файла.
-//  - limit максимальная длина данных события.
+//   - name имя файла. Если он уже существует, то будет переоткрыт на чтение и запись.
+//   - frame размер кадра. Если файл существует, то этот параметр будет взят из файла.
+//   - limit максимальная длина данных события.
 func NewWriter(
 	name string,
 	frame int,
@@ -43,6 +44,7 @@ func NewWriter(
 
 	var file *os.File
 	var res Writer
+	res.wtnid = types.NewIndexAtomic()
 
 	if _, err := os.Stat(name); err != nil {
 		if !os.IsNotExist(err) {
@@ -58,6 +60,7 @@ func NewWriter(
 		if err := writeHeader(file, frame, limit); err != nil {
 			return nil, errors.Wrap(err, "write header into a new file")
 		}
+
 	} else {
 		// Файл существует, читаем параметры frame и limit из него.
 		file, err = os.OpenFile(name, os.O_RDWR, 0644)
@@ -82,6 +85,12 @@ func NewWriter(
 		if err != nil {
 			return nil, errors.Wrap(err, "get existing file stats")
 		}
+
+		lastWrittenID, err := readLastEventID(file, stat, frame)
+		if err != nil {
+			return nil, errors.Wrap(err, "read last event id")
+		}
+		res.wtnid.Set(lastWrittenID)
 
 		if _, err := file.Seek(stat.Size(), 0); err != nil {
 			return nil, errors.Wrap(err, "seek to the file end")
@@ -113,15 +122,72 @@ func NewWriter(
 	return &res, nil
 }
 
+func readLastEventID(file *os.File, stat os.FileInfo, frame int) (id types.Index, err error) {
+	if stat.Size() <= fileMetaInfoHeaderSize {
+		// Файл был создан, но записей в него не было.
+		return id, nil
+	}
+
+	// Получается, запись в файл уже происходила.
+	// Нам нужно узнать индекс последней записи.
+
+	diff := stat.Size() - fileMetaInfoHeaderSize
+	if diff%int64(frame) == 0 {
+		diff--
+	}
+	off := (diff / int64(frame)) * int64(frame)
+	if _, err := file.Seek(off, 0); err != nil {
+		return id, errors.Wrap(err, "seek to the start")
+	}
+
+	rdr := io.LimitReader(file, int64(frame))
+	bufrdr := bufio.NewReader(rdr)
+
+	var buf [16]byte
+	for {
+		if _, err := io.ReadFull(bufrdr, buf[:]); err != nil {
+			switch err {
+			case io.EOF, io.ErrUnexpectedEOF:
+				return id, nil
+			default:
+				if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
+					return id, nil
+				}
+
+				return id, errors.Wrap(err, "read next event data")
+			}
+		}
+
+		nid := types.IndexDecode(buf[:])
+		if nid.Term == 0 {
+			// Предыдущая запись была последней.
+			return id, nil
+		}
+		id = nid
+
+		// Надо пропустить содержимое записи.
+		length, err := binary.ReadUvarint(bufrdr)
+		if err != nil {
+			return id, errors.Wrap(err, "read event data length to pass").Stg("invalid-event-id", id)
+		}
+
+		if _, err := bufrdr.Discard(int(length)); err != nil {
+			return id, errors.Wrap(err, "skip encoded content of the event").Stg("invalid-event-id", id)
+		}
+	}
+}
+
 // Writer писалка логов.
 type Writer struct {
 	dst    *mpio.SimWriter
 	buf    *bytes.Buffer
 	zeroes []byte
+	wtnid  types.IndexAtomic
 
 	frame   uint64
 	limit   int
 	pos     uint64
+	lastid  types.Index
 	bufsize int
 }
 
@@ -159,11 +225,18 @@ func (w *Writer) WriteEvent(id types.Index, data []byte) (int, error) {
 	w.buf.Write(buf[:ll])
 	w.buf.Write(data)
 
-	if _, err := w.dst.WriteFA(w.buf.Bytes()); err != nil {
+	flushed, err := w.dst.WriteFA(w.buf.Bytes())
+	if err != nil {
 		return 0, errors.Wrap(err, "push encoded event data")
+	}
+
+	if flushed {
+		// Данные были сброшены, нужно обновить индекс сброшенной записи.
+		w.wtnid.Set(w.lastid)
 	}
 	deltapos += l
 	w.pos += uint64(deltapos)
+	w.lastid = id
 
 	return deltapos, nil
 }
@@ -183,12 +256,17 @@ func (w *Writer) Flush() error {
 
 // LookupNext поиск события следующего за данным.
 func (w *Writer) LookupNext(id types.Index, logger func(err error)) (LookupResult, error) {
-	if err := w.dst.Flush(); err != nil {
-		// Так как искомые данные могут находиться не в файле, а в буфере.
-		// Пока полагаемся на то, что данная операция (поиска) не будет частой,
-		// особенно в случае сброса с синхронизацией, которая ну никак
-		// не блещет скоростью.
-		return nil, errors.Wrap(err, "flush existing data")
+	if types.IndexLess(w.wtnid.Get(), id) {
+		// Ищется запись с индексом, которая ещё не попала (с гарантией) на диск.
+		// Поиск мы осуществляем только в самом файле, поэтому в данном случае
+		// нужно принудительно сбросить накопленные данные.
+		if err := w.dst.Flush(); err != nil {
+			// Так как искомые данные могут находиться не в файле, а в буфере.
+			// Пока полагаемся на то, что данная операция (поиска) не будет частой,
+			// особенно в случае сброса с синхронизацией, которая ну никак
+			// не блещет скоростью.
+			return nil, errors.Wrap(err, "flush existing data")
+		}
 	}
 
 	res, err := LookupNext(w.dst.Name(), id, logger)
@@ -213,6 +291,8 @@ func (w *Writer) flush() error {
 	if err := w.dst.Flush(); err != nil {
 		return err
 	}
+
+	w.wtnid.Set(w.lastid)
 
 	return nil
 }
